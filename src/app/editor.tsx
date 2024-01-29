d336b8ee19cb9eccd0ee3d35f3dd26eb0c9b9688
Jash Agrawal <agrawaljash99@gmail.com>
2024-01-30 04:45:43 +0530

    init

--- src/app/editor.tsx
+++ src/app/editor.tsx
@@ -157,16 +157,15 @@ const CustomEditor: React.FC = () => {
             <span className="font-bold text-2xl  text-white">Jash Agrawal</span>
           </h1>
 
-          <a
+          <div
             onClick={() => saveContentToLocalStorage()}
-            href="#_"
             className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md my-4"
           >
             <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
             <span className="relative px-4 py-2 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
               <span className="relative text-white text-sm">Save</span>
             </span>
-          </a>
+          </div>
           {/* <button className="px-8 py-2 border border-white rounded-xl my-4">
             Save
           </button> */}
